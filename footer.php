47feff27fbc0b6fe23047f3c3f1772f8d8827430
nidhisilveroak <116626330+nidhisilveroak@users.noreply.github.com>
2022-11-02 16:18:09 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <div>
+        <span>&copy; <script type="text/javascript">
+                var dt = new Date();
+                document.write(dt.getFullYear());
+            </script>
+            <a href="https://web.silveroakhealth.com/" target="_blank">SilverOakHealth.com</a></span>
+    </div>
+
+    <!--
+    <div>
+        <nav class="nav">
+            <a href="https://ewap.silveroakhealth.com/html/terms_of_use.html" target = "_blank" class="nav-link">Terms of Use</a>
+            <a href="https://ewap.silveroakhealth.com/html/privacy_policy.html" target = "_blank" class="nav-link">Privacy Policy</a>
+            <a href="https://web.silveroakhealth.com/contact" target = "_blank" class="nav-link">Contact Us</a>
+        </nav>
+    </div>
+   !-->
+</footer>
